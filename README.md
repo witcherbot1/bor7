e4f0a7b8b9b290e044542542234a696196e7ea91
mohamedeldony3 <mohamedeldony3@gmail.com>
2024-07-18 04:15:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# `⚜𝐒•𝐏•𝐃_𝙱𝙾𝚃⚜` 
+
+# `مرحبا بك في سبيد بوت اول بوت واتساب عربي` 
+<p align="center">
+<img src="https://telegra.ph/file/07278454f366641c0ce34.jpg" alt="⁨𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩" width="900"/>
+</p>
+
+------------------
+> **ملاحظة**: أنا "LIGHT"، تقاعدت من كل ما يتعلق بالبوت أو المواضيع المشابهة، لذا في مرحلة ما سيصبح قديمًا و/أو ستتوقف بعض الأوامر عن العمل.  أنا أقدر تفضيلك خلال الرحلة عبر عالم الروبوتات هذا.
+
+> *⚜𝐒•𝐏•𝐃_𝙱𝙾𝚃⚜* يتم ترك الأمر في أيدي المتعاونين، وباعتباره الممثل الرئيسي 
+------------------
+
+> اللغه الرئيسيه التي اشتغل عليها المطور هي *العربيه*
+
+### `▢ الحسابات الرسمية`
+
+[ 🔗 ] https://atom.bio/harley_light
+
+### `▢ مجموعات الدعم (متوفرة)`
+
+ <a href="https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z" target="blank"><img src="https://img.shields.io/badge/SUPPORT_GROUP_(EN)_(نشط)-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+
+<a href="https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z" target="blank"><img src="https://img.shields.io/badge/SUPPORT_GROUP_(EN)_(نشط)-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+
+<a href="https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z" target="blank"><img src="https://img.shields.io/badge/SUPPORT_GROUP_(EN)_(نشط)-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+
+> [ 📌 ] لا يُسمح بالبوتات الاخري في مجموعات الدعم.
+
+ ### `▢رقم المطور الرسمي (نشط)`
+
+
+<a href="https://api.whatsapp.com/send/?phone=201280244814&text=/estado&type=phone_number&app_absent=0" target="blank"><img src="https://img.shields.io/badge/BOT_OFICIAL_1_(نشط)-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+<a href="https://api.whatsapp.com/send/?phone=201159106301&text=/estado&type=phone_number&app_absent=0" target="blank"><img src="[https://img.shields.io/badge/BOT_OFICIAL_2_(نشط)-25D366?style=for-the-badge&logo=whatsapp&logoColor=white](https://telegra.ph/file/bda9ed97f962db2e7f154.jpg)" />
+
+### `▢يوتيوب المطور الرسمي (نشط)`
+
+<a href="http://www.youtube.com/@HARLEY_247">
+<img src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white" alt="YouTube">
+</a>
+
+### `▢يوتيوب المطور الرسمي (نشط)`
+<a href="http://www.youtube.com/@HARLEY_247">
+<img src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white" alt="YouTube">
+</a>
+
+
+### `▢ المكونات الإضافية`
+- هل لديك أفكار للمكونات الإضافية أو هل ترغب في الحصول على المزيد من المكونات الإضافية؟  ثم قم [انقر هنا]([](https://github.com/theh2so4/Mystic-Plugins))
+
+
+### `▢ نشر في كويب
+
+[![نشر في كويب](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?type=git&repository=https://github.com/BrunoSobrino/TheMystic-Bot-MD&branch=master&name=mysticbot)
+  
+### `▢ نشر في ريبلايت`
+
+[![نشر في ريبلايت](https://repl.it/badge/github/BrunoSobrino/TheMystic-Bot-MD)](https://repl.it/github/BrunoSobrino/TheMystic-Bot-MD) 
+  
+### `▢ نشر في ريندر`
+
+[![نشر في ريندر](https://render.com/images/deploy-to-render-button.svg)](https://dashboard.render.com/blueprint/new?repo=https%3A%2F%2Fgithub.com%2FBrunoSobrino%2FTheMystic-Bot-MD) 
+
+
+#### `▢ نشر في هيروكو `
+لمستخدمي هيروكو
+
+[![نشر في هيروكو](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Botmego/MegoBot-MD1)
---
+++ index.js
@@ -0,0 +1,131 @@
+console.log('[ 💠 ] جاري التجهيز...');
+import {join, dirname} from 'path';
+import {createRequire} from 'module';
+import {fileURLToPath} from 'url';
+import {setupMaster, fork} from 'cluster';
+import {watchFile, unwatchFile} from 'fs';
+import cfonts from 'cfonts';
+import {createInterface} from 'readline';
+import yargs from 'yargs';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const {name, author} = require(join(__dirname, './package.json'));
+const {say} = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
+
+say('The naruto\nBot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']});
+say(`light bot`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']});
+
+let isRunning = false;
+/**
+* Start a js file
+* @param {String} file `path/to/file`
+*/
+function start(file) {
+  if (isRunning) return;
+  isRunning = true;
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+
+  /** say('[ ℹ️ ] Escanea el código QR o introduce el código de emparejamiento en WhatsApp.', {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']}); **/
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1)});
+  const p = fork();
+  p.on('message', (data) => {
+    console.log('[RECIBIDO]', data);
+    switch (data) {
+      case 'reset':
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
+  p.on('exit', (_, code) => {
+    isRunning = false;
+    console.error('[ ℹ️ ] حدث خطأ غير متوقع:', code);
+
+    p.process.kill();
+    isRunning = false;
+    start.apply(this, arguments);
+
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', (line) => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
+}
+
+import fs from 'fs';
+import firebaseAdmin from 'firebase-admin';
+
+// Get DataBase >>
+// حذف ملف database.json إذا كان موجودًا
+try {
+    fs.unlinkSync('database.json');
+    console.log('database.json file deleted successfully.');
+} catch (err) {
+    console.error('Error deleting database.json file:', err);
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync('./firebase-key.json', 'utf8')); // تحميل المفتاح كـ JSON
+const id = serviceAccount.project_id
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: `https://${id}-default-rtdb.firebaseio.com`
+});
+
+// قراءة البيانات من Firebase
+const dbRef = firebaseAdmin.database().ref('/');
+dbRef.once('value', (snapshot) => {
+    const data = snapshot.val();
+
+    const replacedData = replaceInvalidKeys(data);
+
+    fs.writeFileSync('database.json', JSON.stringify(replacedData, null, 4), 'utf8');
+    console.log('Data saved to database.json file successfully.');
+});
+
+function replaceInvalidKeys(obj) {
+    const newObj = {};
+    for (const key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+            const newKey = key.replace(/,/g, '.');
+            newObj[newKey] = obj[key];
+            if (typeof obj[key] === 'object') {
+                newObj[newKey] = replaceInvalidKeys(obj[key]);
+            }
+        }
+    }
+    return newObj;
+}
+
+
+setTimeout(() => {
+    console.log('The next codes are executed after a delay of 26 seconds...');
+    start('main.js')
+}, 26000);
+
+
